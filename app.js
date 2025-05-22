04817ec2662451a8ec9ab0ff5d5e4db4d1126142
권나현 <kwonnahyun0125@gmail.com>
2025-05-22 18:49:53 +0900

    파일업로드드

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
-app.use('/Documents', DocumentsRouter);
+app.use('/Documents/files', express.static('uploads'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/Documents.js
+++ routes/Documents.js
@@ -1,4 +1,61 @@
 var express = require('express');
 var router = express.Router();
+var multer = express.Router();
+var fs = require('fs');
+var path = require('path'); 
+const { db } = require('../utils/db');
+
+
+// 파일 업로드시 uploads 폴더에 저장되도록 설정
+const uploadDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+// multer 미들웨어 설정
+const upload = multer({dest: 'uploads/'});
+
+
+// GET /documents - 모든 문서 리스트 조회
+router.get('/list', async (req, res, next) => {
+  const Docs = await db.Document.findMany({
+    orderBy: {createdAt: 'desc'},
+  });
+  res.json({ requestTime: req.requestTime, Documents: Docs});
+}); 
+
+
+// POST /documents/upload - 파일 업로드
+router.post('/upload',upload.single('file'), async (req, res, next) => {
+  if (!req.file) {
+    return res.status(400).json({error: 'No file uploaded'});
+  }
+
+  const { originalname, filename } = req.file;
+  const extension = path.extname(originalname);
+  const newFileName = `${filename}${extension}`;
+  const newFilePath = path.join(uploadDir, newFileName);
+  fs.renameSync(req.file.path, newFilePath);
+
+  // req.file.originalname;
+  // req.file.mimtype;
+  // req.file.size;
+  // '/Documents/files/${newFileName}';
+  const file = await db.Documen.create({
+    data: {
+      filename: newFileName,
+      mimetype: req.file.mimetype,
+      size: req.file.size,
+      url: `/Documents/files/${newFileName}`,
+    },
+  });
+  // filename String
+  // mimetype  String
+  // size  Int
+  // url String
+
+
+    res.json({message: 'OK'});
+});
 
 module.exports = router;
